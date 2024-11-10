dca1e0c430570206477f694aef68d7c61a76b908
Ιωάννης Τσαντήλας <el20883@mail.ntua.gr>
2024-11-10 15:37:09 +0200

    6th video --review needed

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Button, Grid, Typography, TextField, FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel } from "@material-ui/core";
+import Link from "react-router-dom";
 
 export default class CreateRoomPage extends Component {
   constructor(props) {
@@ -6,6 +8,38 @@ export default class CreateRoomPage extends Component {
   }
 
   render() {
-    return <p>This is the create room page</p>;
+    return <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component="h4" variant="h4">
+          Create A Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControl component="fieldset">
+          <FormHelperText>
+            <div align='center'>Guest Control of Playback State</div>
+          </FormHelperText>
+          <RadioGroup row defaultValue="true">
+            <FormControlLabel
+              value="true"
+              control={<Radio color="primary" />}
+              label="Play/Pause"
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              value="false"
+              control={<Radio color="secondary" />}
+              label="No Control"
+              labelPlacement="bottom"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControlLabel>
+          <TextField required={true} type="number" defaultValue={}/>
+        </FormControlLabel>
+      </Grid>
+    </Grid>;
   }
 }
