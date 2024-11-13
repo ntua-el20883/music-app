92d84ff2d525725918e5b4a6b9024157f163e804
Ιωάννης Τσαντήλας <el20883@mail.ntua.gr>
2024-11-13 15:14:35 +0200

    7th video - host is bydefault false, changed in the room.js to true

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect,} from "react-router-dom";
+import Room from "./Room";
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -23,6 +18,7 @@ export default class HomePage extends Component {
                     </Route>
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/room/:roomCode" component={Room} />
                 </Switch>
             </Router>
         );
